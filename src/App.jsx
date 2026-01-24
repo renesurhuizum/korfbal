515da3f5cefe66bbee7c9c5c156f1311b88b9f0e
renesurhuizum <renedeboer97@gmail.com>
2026-01-24 09:33:24 +0100

    Fix God Mode JSX structure - correct closing tags

--- src/App.jsx
+++ src/App.jsx
@@ -280,7 +280,9 @@ export default function KorfbalApp() {
                   })}
                 </div>
               )}
-            </>
+            </div>
+          </>
+        )}
           </div>
         </div>
       );
